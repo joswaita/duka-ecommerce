4589f272c53ca765ea350e925acbf72b3651599c
joevunga <waitajoseph@gmail.com>
2021-06-10 22:28:59 +0300

    Initial commit

---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,52 @@
+/*import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { TextField, Grid } from '@material-ui/core';
+
+function FormInput({ name, label }) {
+  const { control } = useFormContext();
+  const isError = false;
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+      as={TextField}
+      defaultValue=""
+      control={control}
+      fullWidth
+      name={name}
+      label={label}
+      required
+      />
+    </Grid>
+  );
+}
+
+export default FormInput;
+*/
+import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { TextField, Grid } from '@material-ui/core';
+
+function FormInput({ name, label, required }) {
+  const { control } = useFormContext();
+  const isError = false;
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        control={control}
+        name={name}
+        render={({ field }) => (
+            <TextField
+                fullWidth
+                label={label}
+                required
+      />
+        )}
+        />
+    </Grid>
+  );
+}
+
+
+export default FormInput;
---
+++ src/lib/commerce.js
@@ -0,0 +1,4 @@
+import Commerce from '@chec/commerce.js' ;
+
+
+export const commerce = new Commerce("pk_test_2868228e8077a5a67e43b8d96fc48b70ec1b007df3ecd", true);
